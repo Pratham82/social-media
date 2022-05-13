da5b3a8d782bcb18f0430ca411001b46fefd1735
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-05-14 01:15:18 +0530

    feat: added types

---
+++ types.ts
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+
+export interface IChildrenProps {
+  children?: ReactNode;
+}
+
+export interface IButton {
+  children: React.ReactNode;
+  size?: "medium" | "large" | "xl" | "full";
+  variant?: "primary" | "success" | "danger";
+  onClick?: (e?: any) => any;
+  rounded?: boolean;
+  type?: "button" | "submit" | "reset";
+}
+
+export interface IContainerProps extends IChildrenProps {
+  title?: string;
+}
+
+export interface IListItemProps extends IChildrenProps {
+  id: string | number;
+}
+
+export interface ISidebarItem {
+  id: number;
+  title: string;
+  link: string;
+  icon: JSX.Element;
+}
+
+export interface IPostItem {
+  id: number;
+  icon: JSX.Element;
+}
