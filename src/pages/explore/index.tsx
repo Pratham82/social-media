c4ef509a1b1a18f3fa4b9323d7d042fb937c2dd4
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-05-14 01:20:01 +0530

    feat: added static pages

---
+++ src/pages/explore/index.tsx
@@ -0,0 +1,11 @@
+import Container from "components/container";
+
+const Explore: React.FC = (): JSX.Element => {
+  return (
+    <div className="border-gray-200 dark:border-zinc-800">
+      <Container title="Explore" />
+    </div>
+  );
+};
+
+export default Explore;
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import Container from "components/container";
+
+const Profile: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <Container title="Profile" />
+    </div>
+  );
+};
+
+export default Profile;
