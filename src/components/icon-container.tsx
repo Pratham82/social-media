2f3545bcde00de4f6460ef382dfa7378b1cd5b22
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-05-18 03:07:03 +0530

    fix: fixed z-index of components

--- src/components/icon-container.tsx
+++ src/components/icon-container.tsx
@@ -3,7 +3,7 @@ import { IChildrenProps } from "../../types";
 const IconContainer: React.FC<IChildrenProps> = ({
   children,
 }: IChildrenProps): JSX.Element => (
-  <div className="z-[-1] translate-x-1 scale-90 cursor-pointer rounded-full p-2  transition duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">
+  <div className="translate-x-1 scale-90 cursor-pointer rounded-full p-2  transition duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">
     {children}
   </div>
 );
