b15c889e589c7bde5156f2309f9b9964b91803c2
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-05-18 02:54:40 +0530

    chore: updated next config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["abs.twimg.com", "i.pravatar.cc"],
+    domains: ["abs.twimg.com", "i.pravatar.cc", "res.cloudinary.com"],
   },
   env: {
     GUEST_EMAIL: process.env.GUEST_EMAIL,
     GUEST_PASSWORD: process.env.GUEST_PASSWORD,
+    DEFAULT_PROFILE_IMAGE: process.env.DEFAULT_PROFILE_IMAGE,
   },
 };
